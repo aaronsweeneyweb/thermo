c9e42d695d430922fe8440f37dd3836358ef8a02
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2016-01-25 15:36:42 +0000

    power saving mode is on by default

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -28,5 +28,7 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 
-
+   it('has power saving mode on by default', function () {
+     expect(thermostat.isPowerSavingModeOn()).toBe(true);
+   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,7 @@
 function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
+  this.powerSavingMode = true;
 }
 
 Thermostat.prototype = {
@@ -22,5 +23,9 @@ Thermostat.prototype = {
 
   isMinimumTemperature: function() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
+  },
+
+  isPowerSavingModeOn: function() {
+    return this.powerSavingMode === true;
   }
 };
