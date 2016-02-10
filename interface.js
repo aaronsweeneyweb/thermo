f653586444d025b37227d1fcea512042acfff26b
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2016-02-10 19:23:18 +0000

    openweathermap.org API added

--- interface.js
+++ interface.js
@@ -36,4 +36,16 @@ $(document).ready(function() {
       updateTemprature();
     });
 
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+        $('#current-temperature').text(data.main.temp);
+      });
+    });
+
 });
